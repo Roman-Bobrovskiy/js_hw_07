165a6e861030a87ac6689b12b58f43eb77d58419
RB <bobrovskiyroman@gmail.com>
2021-06-15 12:14:56 +0300

    task1.1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,12 @@
-let h2Ref = document.querySelectorAll(".item");
-// console.log(h2Ref);
-h2Ref.forEach((h2Ref) => {
-  console.log(`Категория: ${h2Ref.children[0].textContent}`);
-  console.log(`В списке ${h2Ref.children[1].children.length} категории`);
+let catRef = document.querySelectorAll(".item");
+// console.log(catRef);
+catRef.forEach((catRef) => {
+  console.log(`Категория: ${catRef.children[0].textContent}`);
+  console.log(`В списке ${catRef.children[1].children.length} категории`);
 });
+// Напиши скрипт, который выполнит следующие операции.
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+// Например для первой категории получится:
+// Категория: Животные
+// Количество элементов: 4
